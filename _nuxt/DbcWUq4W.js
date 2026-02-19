1f1f5716f41449cfac523fbfb82f5b806baf1ce6
Anonymous <username@users.noreply.github.com>
2026-02-19 22:22:19 +0100

    update all
    add novels page

---
+++ _nuxt/DbcWUq4W.js
@@ -0,0 +1,3 @@
+const t=[{id:"n1",title:"第一章：雨过天晴",author:"Alex",date:"2026-02-19",content:`这是第一章正文示例。
+支持多行文本。
+你后续可以换成从后端拉取。`},{id:"n2",title:"第二章：柏林的风",author:"Alex",date:"2026-02-18",content:"第二章正文示例……"}];export{t as n};
